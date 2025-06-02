82703dc500d56e433ad5ca935ed35b97e93edd31
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-01 22:23:40 -0400

    Base API Envio

---
+++ src/main/java/com/api/apienvio/ApienvioApplication.java
@@ -0,0 +1,13 @@
+package com.api.apienvio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApienvioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApienvioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/apienvio/ApienvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apienvio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApienvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
