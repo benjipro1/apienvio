6f605befebff5aa99675615fc85cae2a4a5df4c2
Marco Espinoza <marcotk8@gmail.com>
2025-06-08 19:55:18 -0400

    Se creo el dto del envio

---
+++ src/main/java/com/api/apienvio/dto/EnvioDTO.java
@@ -0,0 +1,15 @@
+package com.api.apienvio.dto;
+
+import lombok.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class EnvioDTO {
+    private Integer IdEnvio;
+    private Date FechaEnvio;
+    private String direccion;
+    private String EstadoEnvio;
+}
