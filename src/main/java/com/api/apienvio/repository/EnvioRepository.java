fea5c52015121e8f5f4874f184b4a8239ff29ad4
Marco Espinoza <marcotk8@gmail.com>
2025-06-08 19:14:30 -0400

    se creo el repositorio de la api y su respectiva clase

---
+++ src/main/java/com/api/apienvio/repository/EnvioRepository.java
@@ -0,0 +1,9 @@
+package com.api.apienvio.repository;
+
+import com.api.apienvio.models.Envio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnvioRepository extends JpaRepository<Envio, Integer> {
+    
+
+}
