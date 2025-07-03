cde9d74c250aaf13ac149294f50030915b9a68fc
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-07-03 13:23:53 -0400

    Se implemento Hateoas

--- src/main/java/com/api/apienvio/controllers/EnvioController.java
+++ src/main/java/com/api/apienvio/controllers/EnvioController.java
@@ -1,11 +1,15 @@
 package com.api.apienvio.controllers;
 
 import com.api.apienvio.dto.EnvioDTO;
+import com.api.apienvio.models.Envio;
 import com.api.apienvio.services.EnvioServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo; 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 @RequestMapping("/api/envios")
@@ -38,5 +42,23 @@ public class EnvioController {
     public ResponseEntity<Void> eliminar(@PathVariable Integer id) {
         envioServices.eliminar(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/hateoas/{id}")
+    public EnvioDTO obtenerHateoas(@PathVariable Integer id) {
+        EnvioDTO dto = envioServices.buscar(id);
+        dto.add(linkTo(methodOn(EnvioController.class).buscar(id)).withSelfRel());
+        dto.add(linkTo(methodOn(EnvioController.class).listar()).withRel("TODOS"));
+        dto.add(linkTo(methodOn(EnvioController.class).eliminar(id)).withRel("ELIMINAR"));
+        return dto;
+    }
+    
+    @GetMapping("/hateoas")
+    public List<EnvioDTO> listarHateoas() {
+        List<EnvioDTO> envios = envioServices.listar();
+        for (EnvioDTO dto : envios) {
+            dto.add(linkTo(methodOn(EnvioController.class).obtenerHateoas(dto.getIdEnvio())).withSelfRel());
+        }
+        return envios;
     }
 }
--- src/main/java/com/api/apienvio/dto/EnvioDTO.java
+++ src/main/java/com/api/apienvio/dto/EnvioDTO.java
@@ -2,12 +2,13 @@ package com.api.apienvio.dto;
 
 import lombok.*;
 import java.sql.Date;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class EnvioDTO {
+public class EnvioDTO extends RepresentationModel<EnvioDTO> {
     private Integer IdEnvio;
     private Date FechaEnvio;
     private String direccion;
